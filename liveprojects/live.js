e80788ea1bf73f2820e03ecbd0ebd4e0fa80b741
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2024-12-19 00:43:56 +0200

    images link

--- liveprojects/live.js
+++ liveprojects/live.js
@@ -3,43 +3,43 @@ const projects = [
     {
         title: "survey-form",
         description: "Web application demonstrating frontend skills",
-        imageUrl: "path/to/project1-image.jpg",
+        imageUrl: "https://img.freepik.com/free-vector/feedback-survey-concept-illustration_114360-15500.jpg?t=st=1734560663~exp=1734564263~hmac=c3c1ebe5ac51f42596d56dc31b860e1627acaa699569a0ba9d226115d1b82dea&w=1060",
         liveLink: "https://survey-form-yrkk.onrender.com",
         codeLink: "https://github.com/Derrick-MUGISHA/survey-form"
     },
     {
         title: "Build-a-Tribute-Page-Assignment-",
         description: "Build-a-Tribute-Page-Assignment-",
-        imageUrl: "path/to/project2-image.jpg",
+        imageUrl: "https://build-a-tribute-page-assignment.onrender.com/image/tribute-page-main-image.jpg",
         liveLink: "https://build-a-tribute-page-assignment.onrender.com",
         codeLink: "https://github.com/Derrick-MUGISHA/Build-a-Tribute-Page-Assignment-"
     },
     {
         title: "Responsive-animated-page",
         description: "Responsive-animated-page",
-        imageUrl: "path/to/project3-image.jpg",
+        imageUrl: "https://responsive-animated-page.onrender.com/img-20241202T113414Z-001/img/hero.png",
         liveLink: "https://responsive-animated-page.onrender.com",
         codeLink: "https://github.com/Derrick-MUGISHA/Responsive-animated-page"
     },
     {
         title: "magic-ball",
         description: "UI/UX design implementation",
-        imageUrl: "path/to/project4-image.jpg",
+        imageUrl: "https://magic-8ball.com/wp-content/uploads/ball.avif",
         liveLink: "https://magic-ball-7dze.onrender.com",
         codeLink: "https://github.com/Derrick-MUGISHA/magic-ball"
     },
     {
         title: "Project Five",
         description: "Complex web application",
-        imageUrl: "path/to/project5-image.jpg",
-        liveLink: "#",
+        imageUrl: "https://w3schools-bunb.onrender.com/images/man_smoke.jpg",
+        liveLink: "https://w3schools-bunb.onrender.com/",
         codeLink: "https://github.com/Derrick-MUGISHA/list-and-search-filiter"
     },
     {
-        title: "Project Six",
-        description: "Frontend development project",
-        imageUrl: "path/to/project6-image.jpg",
-        liveLink: "#",
+        title: "technical documentation",
+        description: "technical-documentation-page",
+        imageUrl: "https://www.dynaway.com/hs-fs/hubfs/Blog%20Featured%20Image.jpg?width=1040&height=684&name=Blog%20Featured%20Image.jpg",
+        liveLink: "https://technical-documentation-page-2lfw.onrender.com/",
         codeLink: "#"
     },
     {
